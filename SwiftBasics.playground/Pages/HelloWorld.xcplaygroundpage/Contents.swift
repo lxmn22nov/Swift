3202631d3b05bc5a639d36c5c0c715959f35f1a8
Laxman Singh Koranga <lxmn@Laxman-MacBook-Pro.local>
2023-09-22 00:55:02 +0530

    added SwiftBasicsPlayground

---
+++ SwiftBasics.playground/Pages/HelloWorld.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/* print() function outputs one or more values to the terminal. */
+print("Hello, Swift!")
+
+// Program Structure.
+/* The program runs line by line, from top to bottom. */
+print("I'm Laxman Singh Koranga.")
+print("Computer Application Graduate.")
+
+// Single-line Comment.
+// Single-line comments are created using two consecutive forward slashes.
+// The compiler ignores any text after // on the same line.
+
+/* Multiline Comment. */
+/* Multiline comments are created using /* to begin the comment and */ to end the comment.
+ The compiler ignores any text in between. */
