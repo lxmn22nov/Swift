3bfc79994b0a95bc1bbaf68547f0f9b4eb23ac10
Laxman Singh Koranga <lxmn@Laxman-MacBook-Pro.local>
2023-09-23 22:17:09 +0530

    Initial commit

--- SwiftBasics.playground/Pages/Basics.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -1,2 +1,38 @@
-// Constants and Variables.
+// Variables.
+/* A variable refers to a storage location in the computer’s memory that one can set aside to save, retrieve, and manipulate data. */
+var a = 5
+var b = "Captain America"
+print(a)
+print(b)
+
+// Constants.
+/* Constants refer to fixed values that a program may not alter during its execution. One can be declared by using the let keyword. */
+let pi = 3.14
+print(pi)
+
 // Type Annotations.
+/* To be clear about the kind of values the constant or variable can store. */
+/* After variable and constant use (:) and declare the particular type of data-type. */
+var a1: Int = 5
+var b1: String = "Captain America"
+print(a1)
+print(b1)
+
+let pi1: Float = 3.14
+print(pi1)
+
+/* Constants and Variables can't contain:
+ - Whitespace characters.
+ - Mathematical symbols.
+ - Arrows.
+ - Unicode scalar values.
+ - cannot begin with numbers.
+ */
+
+// Semi-Colans:
+/* Unlike other programming languages, Swift doesn't use (;) after each statement, although we can do it, if we want to but this will not be the good practice. */
+var name: String = "Scarlett Johanson"
+var name1: String = "Chris Evans";
+print("Name without semicolan in the statement:",name)
+print("Name with the semicolan in the statement:",name1);
+
