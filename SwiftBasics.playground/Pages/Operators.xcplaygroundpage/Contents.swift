37de75a9c3bd04db9c8dc1b7086d792f64bddfdc
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-09-27 16:05:34 +0530

    Added Basic Operators in Swift.

--- SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -1,1 +1,48 @@
-/* Basic Operators */
+// Basic Operators
+/*
+ An operator is a special symbol or phrase that you use to check, change, or combine values.
+ Eg: the addition operator (+) adds two numbers and the logical AND operator (&&) combines two Boolean values.
+*/
+let a = 1 + 2
+print("Addition:",a)
+// Unary Operators: operates on single operand(eg: -a, !a, a!).
+var Three = 3
+var minusThree = -Three
+var plusThree = -minusThree
+print("Three",Three)
+print("Minus Three:",minusThree)
+print("Plus Three:",plusThree)
+
+// Ternary Operators: operates on triple operand(eg: a ? b : c)
+// Assignment Operator: initializes or updates the value of operands.
+var b = 3
+var c = 6
+b = c
+print("value of b:",b)
+print("value of c:",c)
+
+// Arithmetic Operators.
+// Binary Operators: operates on double operand(eg: a + b, a - b, a * b, a / b)
+var x1 = 3 + 2
+print("Addition:",x1)
+
+var x2 = 3 - 2
+print("Subtraction:",x2)
+
+var x3 = 3 * 3
+print("Multiplication:",x3)
+
+var x4 = 5 / 5
+print("Division:",x4)
+
+var x5 = 9 % 4
+print("Remainder Operator:",x5)
+
+var x6 = -9 % 4
+print("Remainder Operator:",x6)
+
+// NOTE: (+) operator is also used to concatenate the strings.
+var str = "Hello, " + "Swift"
+print("Concatenation:",str)
+
+
